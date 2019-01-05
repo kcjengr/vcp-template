d76eaf0f4be1be331ab8ebf23a6403cb151a213c
Kurt Jacobson <kcjengr@gmail.com>
2019-01-05 17:53:11 -0500

    add JT's copy script

---
+++ copy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo Enter NEW configuration name
+read CONFIG_NAME
+DIR="/home/$USER/linuxcnc/qtpyvcp/${CONFIG_NAME,,}"
+echo $DIR
+SUBDIR="/home/$USER/linuxcnc/qtpyvcp/${CONFIG_NAME,,}/${CONFIG_NAME,,}"
+echo $SUBDIR
+
+function quit {
+	exit
+}
+
+function create {
+	echo Creating the Directory $DIR
+	mkdir -p $DIR
+	echo Copying the Files
+	echo $PWD
+	cp -r $PWD/myvcp $DIR/${CONFIG_NAME,,}
+	sed -i.bak "s/myvcp/${CONFIG_NAME,,}/g; s/MyVCP/$CONFIG_NAME/g" $SUBDIR/config.yml
+	sed -i.bak "s/myvcp/${CONFIG_NAME,,}/g; s/MyVCP/$CONFIG_NAME/g" $SUBDIR/__init__.py
+	cp $PWD/setup.py $DIR
+	sed -i.bak "s/myvcp/${CONFIG_NAME,,}/g; s/MyVCP/$CONFIG_NAME/g" $DIR/setup.py
+	cp $PWD/README.md $DIR
+	cp $PWD/MANIFEST.in $DIR
+	cp $PWD/LICENSE $DIR
+	cd $DIR
+	set -x
+	pip install -e .
+}
+
+if [ -d "$DIR" ]; then
+  # Control will enter here if $DIR exists.
+	echo The $DIR exists! Delete it and start again? Yes No
+	read REPLY
+	if [ ${REPLY,,} == "yes" ]; then
+		create
+	else
+		quit
+	fi
+else
+	create
+fi
